8e5e9162049d2f7052981dd5a369c31094ee2f9a
Jinhui Liu <jinhuiliu@JinhuideMacBook-Pro.local>
2018-06-03 11:42:04 +1200

    mute button
    mute button

---
+++ Space Frontier/Assets/_MyScripts/SoundButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour {
+	
+	public GameObject soundControlButton;
+	public Sprite audioOffSprite;
+	public Sprite audioOnSprite;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		if (AudioListener.pause == true) {
+			soundControlButton.GetComponent<Image> ().sprite = audioOffSprite;
+		} else {
+			
+			soundControlButton.GetComponent<Image> ().sprite = audioOnSprite;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void SoundControl(){
+		if (AudioListener.pause == true) {
+			AudioListener.pause = false;
+			soundControlButton.GetComponent<Image> ().sprite = audioOnSprite;
+		} else {
+			AudioListener.pause = true;
+			soundControlButton.GetComponent<Image> ().sprite = audioOffSprite;
+		}
+
+}
+}
